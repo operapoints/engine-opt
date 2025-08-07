e892f58128eef28b4a24cbebec8514eadfcf2514
Ethan <ethan26@mit.edu>
2025-08-06 19:56:11 -0700

    Add mass model

--- jet_calc.cpp
+++ jet_calc.cpp
@@ -176,8 +176,9 @@ vector_double problem_jet_calc::fitness(const vector_double &x) const{
         double Ts_To = T_5 - (u_Toa*u_Toa)/(2*C_ph);
         double con_turbine_outlet_width = 0.009 - A_To/(2*M_PI*R_Tom); // Turbine outlet width more than 9mm
         double R_Tot = R_Tom + A_To/(4*M_PI*R_Tom);
-        double M_Tom = std::pow((u_Toa*u_Toa + omega*omega*R_Tot*R_Tot)/(gam_h*R*Ts_To),0.5);
+        double M_Tom = std::pow((u_Toa*u_Toa + omega*omega*R_Tom*R_Tom)/(gam_h*R*Ts_To),0.5);
         double con_M_Tom = M_Tom - 0.8; // Relative Mach at turbine exit less than 0.8
+        double M_Toa = u_Toa / std::pow((gam_h*R*Ts_To),0.5);
         double sigma_max_Ti = 0.5*omega*omega*rho_C*(R_Tit*R_Tit - R_Tih*R_Tih);
         double con_sigma_max_Ti = FOS_T * sigma_max_Ti - sigma_max_T;// FOS at compressor inlet blade root
         double R_Toh = R_Tom - A_To/(4*M_PI*R_Tom);
@@ -210,11 +211,33 @@ vector_double problem_jet_calc::fitness(const vector_double &x) const{
         double Ts_6 = T_5*std::pow(Ps_6/P_5,(gam_h-1)/gam_h);
         double a_6 = std::pow(gam_h*R*Ts_6,0.5);
         double u_6 = a_6*std::pow((2/(gam_h-1))*(std::pow((P_5/Ps_6),(gam_h-1)/gam_h)-1),0.5);
+        double M_6 = u_6/a_6;
         #ifdef EVAL_JET_CALC
         double R_6 = std::pow((m_dot*(1+f))/(u_6*(Ps_0/(R*Ts_6)))/M_PI,0.5);
         #endif
         double F = m_dot*((1+f)*u_6 - u_0);
         double con_F = 70-F; // Thrust at least 70N
+        // Mass model
+        double m_Compressor = std::abs((M_PI*(R_Cih*R_Cih*R_Cih - R_Coh*R_Coh*R_Coh)))*(rho_C/3);
+        double m_Turbine = std::abs((M_PI*(R_Tih*R_Tih*R_Tih - R_Toh*R_Toh*R_Toh)))*(rho_T/3);
+        // Diffuser mass proportional to spec speed and component radius cubed
+        double m_Diffuser = (0.07146/(0.03*0.03*0.03))*(R_Com*R_Com*R_Com)*(0.4/spec_speed_C);
+        double m_NGV = (0.2885/(0.0315287*0.0315287*0.0315287))*(R_Tit*R_Tit*R_Tit);
+
+        double m_Duct = 2*M_PI*R_Com*R_Com*0.001*2700;
+        double m_Nozzle = 2*M_PI*R_Tit*R_Tot*0.001*8000;
+
+        double len_Combustor = u_Tia * 0.0005 + 0.01;
+        double m_Shaft = (len_Combustor+R_Com+0.02) * M_PI * 0.005 * 0.005 * 8000;
+        double R_overall = (R_Com * (0.036/0.03) * (0.4/spec_speed_C));
+        double m_Wall = 2* M_PI * R_overall * (len_Combustor + 0.02) * 0.001 * 8000;
+        double m_Combustor = 2*M_PI*(0.5*R_overall + 0.8*R_overall)*len_Combustor*0.001;
+
+        double m_total = 1.3*(m_Compressor + m_Turbine+
+                        m_Diffuser + m_NGV + m_Duct+
+                        m_Nozzle+m_Shaft+m_Wall+m_Combustor);
+
+
         //Calculate objective
         double Isp = (F/(m_dot*f*9.8066));
         vector_double ret = {-(F/75.2493+Isp/2493.95), 
